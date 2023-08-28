6f863a62293d3e6e4a1d0da716b20aefaf2e54e9
Dmytry Bondariev <bondarievmd@gmail.com>
2023-08-28 20:16:31 +0300

    Added doc string to start function

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=["start"])
+'''Function to start the bot'''
 async def start(message: types.Message):
     await message.answer(f"Hello, {message.from_user.first_name}!")
 
