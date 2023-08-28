e71dfcd808e74e0d129c77e85e575f8e64c4ff5b
Glieb <gleblitvinenko3007@gmail.com>
2023-08-28 19:41:01 +0300

    Installed isort & black | added blank line

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
-from aiogram import Bot, Dispatcher, types, executor
-from dotenv import load_dotenv
 import os
+
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -8,9 +9,10 @@ bot = Bot(token=os.getenv("BOT_TOKEN"))
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=["start"])
 async def start(message: types.Message):
     await message.answer("Start")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
