1f623dd8287e44755e92076b1304c91d515f3cbb
Glieb <gleblitvinenko3007@gmail.com>
2023-08-30 10:01:00 +0300

    Created item & item category tables installer. Also added statement "IF NOT EXISTS" to user table.

--- db_installer.py
+++ db_installer.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 CREATE_USER_TABLE = """
-    CREATE TABLE "user" (
+    CREATE TABLE IF NOT EXISTS "user" (
         "id" INTEGER NOT NULL,
         "telegram_id" INTEGER NOT NULL UNIQUE ,
         "phone_number" VARCHAR NULL,
@@ -14,11 +14,32 @@ CREATE_USER_TABLE = """
 """
 
 
+CREATE_ITEM_CATEGORY_TABLE = """
+    CREATE TABLE IF NOT EXISTS "item category" (
+    "id" INTEGER PRIMARY KEY,
+    "title" VARCHAR UNIQUE
+    )
+"""
+
+
+CREATE_ITEM_TABLE = """
+    CREATE TABLE IF NOT EXISTS "item" (
+    "id" INTEGER PRIMARY KEY,
+    "item_id" INTEGER NOT NULL UNIQUE,
+    "category" INTEGER,
+    "price" DECIMAL,
+    FOREIGN KEY ("category") REFERENCES "item category" ("id")
+    )
+"""
+
+
 def create_db() -> None:
     connection = sqlite3.connect("cosmetics_shop.db")
     cursor = connection.cursor()
     try:
         cursor.execute(CREATE_USER_TABLE)
+        cursor.execute(CREATE_ITEM_CATEGORY_TABLE)
+        cursor.execute(CREATE_ITEM_TABLE)
         connection.commit()
     except Exception as e:
         print(e)
