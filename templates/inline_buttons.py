d843987f6afbc54287a860cbd3934e39798508de
Glieb <gleblitvinenko3007@gmail.com>
2023-08-31 11:38:10 +0300

    Implemented inline markup generator

---
+++ templates/inline_buttons.py
@@ -0,0 +1,24 @@
+from aiogram import types
+
+
+def generate_inline_markup(
+    button_titles: list,
+    row_width: int
+) -> types.InlineKeyboardMarkup:  # TODO pagination implementation
+    markup = types.InlineKeyboardMarkup(row_width=row_width)
+    row = []
+
+    for button_title in button_titles:
+        button = types.InlineKeyboardButton(
+            f"{button_title}", callback_data=f"{button_title}_cb_data"
+        )
+        row.append(button)
+
+        if len(row) == row_width:
+            markup.row(*row)
+            row.clear()
+
+    if row:
+        markup.row(*row)
+
+    return markup
