f415dacb81f685da0c88bf194e45f1482b97a575
Dmytry Bondariev <bondarievmd@gmail.com>
2023-08-31 08:20:03 +0300

    Added inline_buttons templates; added share phone number keyboard reply markup

--- bot.py
+++ bot.py
@@ -12,53 +12,57 @@ load_dotenv()
 bot = Bot(token=os.getenv("BOT_TOKEN"))
 dp = Dispatcher(bot)
 
-user_data = {}
+# user_data = {}
 
 user = User()
 
 
+# ToDo: make bot parse user phone number and save it to db, if hidden - ask user to share contacts
+# ToDo: make Profile inline button prototype
 @dp.message_handler(commands=["start"])
 async def start(message: types.Message):
     if not user.user_exists(message.from_user.id):
         user.create_user(message.from_user.id)
+        await message.answer(f"Welcome, {message.from_user.username}!")
+    else:
+        await message.answer(f"Welcome back, {message.from_user.username}!")
+    phone_number = message.contact.phone_number if message.contact else None
+    if not phone_number:
         await message.answer(
-            f"Welcome, {message.from_user.username}! Let's get to know you better."
+            "Please share your phone number with me, so I can contact you if needed.",
+            reply_markup=types.ReplyKeyboardMarkup(resize_keyboard=True).add(
+                types.KeyboardButton("Share phone number", request_contact=True)
+            ),
         )
-        await message.answer("Please provide your first name:")
-        user_data[message.from_user.id] = {"state": "waiting_first_name"}
+        # await message.answer("Please provide your first name:")
+        # user_data[message.from_user.id] = {"state": "waiting_first_name"}
 
 
-@dp.message_handler(
-    lambda message: user_data.get(message.from_user.id, {}).get("state")
-    == "waiting_first_name"
-)
-async def get_first_name(message: types.Message):
-    user_data[message.from_user.id]["first_name"] = message.text
-    user.update_profile(message.from_user.id, first_name=message.text)
-    await message.answer("Great! Now please provide your last name:")
-    user_data[message.from_user.id]["state"] = "waiting_last_name"
-
-
-@dp.message_handler(
-    lambda message: user_data.get(message.from_user.id, {}).get("state")
-    == "waiting_last_name"
-)
-async def get_last_name(message: types.Message):
-    user_data[message.from_user.id]["last_name"] = message.text
-    user.update_profile(message.from_user.id, last_name=message.text)
-    await message.answer("Awesome! Now please provide your phone number:")
-    user_data[message.from_user.id]["state"] = "waiting_phone_number"
-
-
-@dp.message_handler(
-    lambda message: user_data.get(message.from_user.id, {}).get("state")
-    == "waiting_phone_number"
-)
-async def get_phone_number(message: types.Message):
-    user_data[message.from_user.id]["phone_number"] = message.text
-    user.update_profile(message.from_user.id, phone_number=message.text)
-    await message.answer("Thank you! Your profile is complete.")
-    user_data[message.from_user.id]["state"] = "complete"
+# @dp.message_handler(
+#     lambda message: user_data.get(message.from_user.id, {}).get("state") == "waiting_first_name")
+# async def get_first_name(message: types.Message):
+#     user_data[message.from_user.id]["first_name"] = message.text
+#     user.update_profile(message.from_user.id, first_name=message.text)
+#     await message.answer("Great! Now please provide your last name:")
+#     user_data[message.from_user.id]["state"] = "waiting_last_name"
+#
+#
+# @dp.message_handler(
+#     lambda message: user_data.get(message.from_user.id, {}).get("state") == "waiting_last_name")
+# async def get_last_name(message: types.Message):
+#     user_data[message.from_user.id]["last_name"] = message.text
+#     user.update_profile(message.from_user.id, last_name=message.text)
+#     await message.answer("Awesome! Now please provide your phone number:")
+#     user_data[message.from_user.id]["state"] = "waiting_phone_number"
+#
+#
+# @dp.message_handler(
+#     lambda message: user_data.get(message.from_user.id, {}).get("state") == "waiting_phone_number")
+# async def get_phone_number(message: types.Message):
+#     user_data[message.from_user.id]["phone_number"] = message.text
+#     user.update_profile(message.from_user.id, phone_number=message.text)
+#     await message.answer("Thank you! Your profile is complete.")
+#     user_data[message.from_user.id]["state"] = "complete"
 
 
 @dp.message_handler(commands=["test_categories"])
