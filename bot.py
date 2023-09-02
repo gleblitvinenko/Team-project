d2af895e1733fe51fb7038706242510d854a1551
Dmytry Bondariev <bondarievmd@gmail.com>
2023-09-02 15:25:42 +0300

    Added categories logic from Gleb repo

--- bot.py
+++ bot.py
@@ -54,92 +54,55 @@ async def set_phone_number(message: types.Message):
     )
 
 
+@dp.message(Command("test_categories"))
+async def test_categories(message: types.Message):
+    """TEST FUNCTION"""
+    item_categories_manager = ItemCategory()
+    item_categories_list = item_categories_manager.get_titles()
+    item_categories_markup = generate_inline_markup(
+        item_categories_list, row_width=2, button_type="category"
+    )
+    await message.answer(
+        "Here is categories", reply_markup=item_categories_markup.as_markup()
+    )
+
+
+@dp.callback_query(F.data.endswith("_cb_data"))
+async def show_items_based_on_category(callback_query: types.CallbackQuery):
+    """HANDLER FOR ITEMS & ITEM CATEGORY BUTTONS"""
+    item_manager = Item()
+    if callback_query.data.endswith("_cat_cb_data"):
+        category_title = callback_query.data.split("_", 1)[0]
+
+        item_titles_list = item_manager.get_items_titles_list_by_category_title(
+            category_title=category_title
+        )
+        items_markup = generate_inline_markup(
+            button_titles=item_titles_list, row_width=2, button_type="item"
+        )
+        await callback_query.message.answer(
+            text=f"{callback_query.data.split('_', 1)[0]} category items",
+            reply_markup=items_markup.as_markup(),
+        )
+    elif callback_query.data.endswith("_item_cb_data"):
+        item_title = callback_query.data.split("_", 1)[0]
+        item_details_dict = item_manager.get_item_details_dict_by_item_title(
+            item_title=item_title
+        )
+        await callback_query.message.answer(
+            text=f"""
+ℹ️ Item Title: {item_details_dict.get("title")}
+
+🆔 ID: {item_details_dict.get("id")}
+
+💰 Price: {item_details_dict.get("price")}
+""",
+        )
+
+
 async def main():
-    # await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     asyncio.run(main())
-# @dp.message_handler(commands=["start"])
-# async def start(message: types.Message):
-#     markup = types.InlineKeyboardMarkup(row_width=1)
-#     profile_button = types.InlineKeyboardButton(text="Profile", callback_data="profile")
-#     markup.add(profile_button)
-#     if not user.user_exists(message.from_user.id):
-#         user.create_user(message.from_user.id)
-#     phone_number = user.check_field(message.from_user.id, "phone_number")
-#     if not phone_number:
-#         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#         keyboard.add(
-#             types.KeyboardButton(text=share_phone_number_inline, request_contact=True)
-#         )
-#
-#         await message.answer(
-#             "Please share your phone number with me.", reply_markup=keyboard
-#         )
-#     else:
-#         await message.answer(
-#             f"Welcome, {message.from_user.username}!", reply_markup=markup
-#         )
-#
-
-# @dp.message_handler(
-#     content_types=types.ContentType.CONTACT,
-# )
-# async def get_phone_number(message: types.Message):
-#     user.update_profile(message.from_user.id, phone_number=message.contact.phone_number)
-#
-#     await message.answer(
-#         "Thank you! Your phone number has been saved.",
-#         reply_markup=types.ReplyKeyboardRemove(),
-#     )
-
-
-# @dp.message_handler(commands=["test_categories"])
-# async def test_categories(message: types.Message):
-#     """TEST FUNCTION"""
-#     item_categories_manager = ItemCategory()
-#     item_categories_list = item_categories_manager.get_titles()
-#     item_categories_markup = generate_inline_markup(
-#         item_categories_list, row_width=2, button_type="category"
-#     )
-#     await message.answer("Here is categories", reply_markup=item_categories_markup)
-#
-#
-# @dp.callback_query_handler(lambda query: "_cb_data" in query.data)
-# async def show_items_based_on_category(callback_query: types.CallbackQuery):
-#     """HANDLER FOR ITEMS & ITEM CATEGORY BUTTONS"""
-#     item_manager = Item()
-#     if callback_query.data.endswith("_cat_cb_data"):
-#         category_title = callback_query.data.split("_", 1)[0]
-#
-#         item_titles_list = item_manager.get_items_titles_list_by_category_title(
-#             category_title=category_title
-#         )
-#         items_markup = generate_inline_markup(
-#             button_titles=item_titles_list, row_width=2, button_type="item"
-#         )
-#         await bot.send_message(
-#             callback_query.from_user.id,
-#             text=f"{callback_query.data.split('_', 1)[0]} category items",
-#             reply_markup=items_markup,
-#         )
-#     elif callback_query.data.endswith("_item_cb_data"):
-#         item_title = callback_query.data.split("_", 1)[0]
-#         item_details_dict = item_manager.get_item_details_dict_by_item_title(
-#             item_title=item_title
-#         )
-#         await bot.send_message(
-#             callback_query.from_user.id,
-#             text=f"""
-# ℹ️ Item Title: {item_details_dict.get("title")}
-#
-# 🆔 ID: {item_details_dict.get("id")}
-#
-# 💰 Price: {item_details_dict.get("price")}
-# """,
-#         )
-
-
-# executor.start_polling(dp, skip_updates=True)
